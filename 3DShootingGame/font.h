761b0aab907c49c0543b5db32d69196c98014218
Tsuyoshi Katayama <katayama933@gmail.com>
2022-03-30 23:49:33 +0900

    関数の追加。fontBegin(), fontEnd(), fontPosition(float _x, float _y), fontDraw(const char* format, ...) 構造体のコメント追加

--- 3DShootingGame/font.h
+++ 3DShootingGame/font.h
@@ -1,54 +1,64 @@
 #pragma once
+#ifndef _FONT_H_
+#define _FONT_H_
 
-typedef struct {
-	char fase[64];
-	unsigned int size;
-	unsigned int bold;
-	unsigned int italic;
-	char charset[64];
-	unsigned int unicode;
-	unsigned int stretchH;
-	unsigned int smooth;
-	unsigned int aa;
-	unsigned int padding[4];
-	unsigned int spacing[2];
-	unsigned int outline;
+/* This tag holds information on how the font was generated. */
+typedef struct tagFONTINFO{
+	short fontSize;
+	unsigned char bitField;
+	unsigned char charSet;
+	unsigned short stretchH;
+	unsigned char aa;
+	unsigned char paddingUp;
+	unsigned char paddingRight;
+	unsigned char paddingDown;
+	unsigned char paddingLeft;
+	unsigned char spacingHoriz;
+	unsigned char spasingVert;
+	unsigned char outline;
+	char fontName[256];
 }FONTINFO;
 
-typedef struct {
-	unsigned int lineHeight;
-	unsigned int base;
-	unsigned int scaleW;
-	unsigned int scaleH;
-	unsigned int page;
-	unsigned int packed;
-	unsigned int alphaChnl;
-	unsigned int redChnl;
-	unsigned int greenChnl;
-	unsigned int blueChnl;
+/* This tag holds information common to all characters. */
+typedef struct tagFONTCOMMON{
+	unsigned short lineHeight;
+	unsigned short base;
+	unsigned short scaleW;
+	unsigned short scaleH;
+	unsigned short pages;
+	unsigned char bitField;
+	unsigned char alphaChnl;
+	unsigned char redChnl;
+	unsigned char greenChnl;
+	unsigned char blueChnl;
 }FONTCOMMON;
 
-typedef struct {
-	unsigned int id;
-	char file[256];
+/* This tag gives the name of a texture file. There is one for each page in the font. */
+typedef struct tagFONTPAGE{
+	char pageNames[256];
 }FONTPAGE;
 
-typedef struct {
-	unsigned int count;
-}FONTCHARS;
-
-typedef struct {
-	unsigned int id;
-	unsigned int x;
-	unsigned int y;
-	unsigned int width;
-	unsigned int height;
-	unsigned int xoffset;
-	unsigned int yoffset;
-	unsigned int xadvance;
-	unsigned int page;
-	unsigned int chnl;
+/* This tag describes on character in the font. There is one for each included character in the font. */
+typedef struct tagFONTCHAR{
+	unsigned int id;		// The character id
+	unsigned short x;		// The left position of the character image in the texture
+	unsigned short y;		// The top position of the character image in the texture
+	unsigned short width;	// The width of the character image in the texture
+	unsigned short height;	// The height of the character image in the texture
+	short xoffset;			// How much the current position should be offset when copying the image from the texture to the screen
+	short yoffset;			// How much the current position should be offset when copying the image from the texture to the screen
+	short xadvance;			// How much the current position should be advanced after drawing the character.
+	unsigned char page;		// The texture page where the character image is found
+	unsigned char chnl;		// The texture channel where the character image is found (1 = blue, 2 = green, 4 = red, 8 = alpha, 15 = all channels)
 }FONTCHAR;
 
-int fontInit(const char* _fileName);
+int fontInit();
 void fontRelease();
+
+void fontBegin();
+void fontEnd();
+void fontPosition(float _x, float _y);
+
+void fontDraw(const char* format, ...);
+
+#endif // !_FONT_H_
